21fe925ae7c18e18a9541c9b10050446a3b26e6f
_earthtospace <niema.widaha@gmail.com>
2022-09-13 23:04:34 +0100

    wallet done

---
+++ client/src/pages/DustPage.tsx
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar'
+
+function DustPage(){
+  return(
+  <div className="h-screen w-screen flex bg-black">
+    <div className="relative w-full h-full bg-black">
+      <NavBar></NavBar>
+    </div>
+  </div>);
+  }
+  
+  export default DustPage;
---
+++ client/src/pages/ExchangePage.tsx
@@ -0,0 +1,13 @@
+import NavBar from '../components/NavBar'
+
+function ExchangePage() {
+  return (
+    <div className="h-screen w-screen flex bg-black">
+        <div className="relative w-full h-full bg-black">
+          <NavBar></NavBar>
+        </div>
+    </div> 
+    );
+}
+
+export default ExchangePage;
