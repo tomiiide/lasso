2d72fbc0514bde57fd0b7c431758bf9bad4ddaf8
tomide <ayotomiiide@gmail.com>
2022-09-14 08:40:51 +0100

    ft: add valid links

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,27 +1,45 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import logo from "../logo.png"
+import logo from "../logo.png";
 
 function NavBar() {
-    return (
-      <div className="navbar bg-black relative z-50">
+  return (
+    <div className="navbar bg-black relative z-50">
       <div className="px-4 navbar-start">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src={logo} alt="#_"></img>
-        </div>
-      </label>
-        <a href="/" className="btn btn-ghost normal-cas text-white text-xl">lasso</a>
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img src={logo} alt="#_"></img>
+          </div>
+        </label>
+        <a href="/" className="btn btn-ghost normal-cas text-white text-xl">
+          lasso
+        </a>
         <ul className="menu menu-horizontal p-0">
-          <li><a href="/dustsweeper"className="text-bold text-white hover:bg-green-300 hover:text-black">Dust Sweeper</a></li>
-          <li><a href="/exchange" className="text-bold text-white hover:bg-green-300 hover:text-black">Exchange</a></li>
-          <li><a href="/marketplace" className="text-bold text-white hover:bg-green-300 hover:text-black">Marketplace</a></li>
+          <li>
+            <a
+              href="/dustsweeper"
+              className="text-bold text-white hover:bg-green-300 hover:text-black"
+            >
+              Dust Sweeper
+            </a>
+          </li>
+          {/* <li><a href="/exchange" className="text-bold text-white hover:bg-green-300 hover:text-black">Exchange</a></li> */}
+          <li>
+            <a
+              href="https://lasso-donations.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-bold text-white hover:bg-green-300 hover:text-black"
+            >
+              Marketplace
+            </a>
+          </li>
         </ul>
       </div>
       <div className="navbar-end py-4 px-4">
-      <ConnectButton></ConnectButton>
+        <ConnectButton></ConnectButton>
       </div>
-      </div>
-      );
-  }
-  
-  export default NavBar;
+    </div>
+  );
+}
+
+export default NavBar;
